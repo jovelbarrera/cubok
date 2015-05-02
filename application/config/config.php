9889bc2013c2b14ca3e6963b5075915d0c2c3aaa
rejbarrera <rej.barrera@outlook.com>
2015-05-01 23:02:21 -0600

    fix error try

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= './';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'Urci0MF5ynS2538gI6G7rE740tTFB6Hv';
+$config['encryption_key'] = '2kVKtLaa5i7F4mixXdGFH0SsuSuOtJ8f';
 
 /*
 |--------------------------------------------------------------------------
@@ -244,13 +244,13 @@ $config['encryption_key'] = 'Urci0MF5ynS2538gI6G7rE740tTFB6Hv';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_cookie_name']	= 'cubok';
+$config['sess_expiration']	= 7200;
 $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
-$config['sess_use_database']	= TRUE;
-$config['sess_table_name']		= 'ci_sessions';
-$config['sess_match_ip']		= FALSE;
+$config['sess_use_database']	= FALSE;
+$config['sess_table_name']	= 'ci_sessions';
+$config['sess_match_ip']	= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
 
--- application/views/header.php
+++ application/views/header.php
@@ -22,9 +22,8 @@ and open the template in the editor.
         <link rel="icon" type="image/png" sizes="32x32" href="<?php echo asset_url(); ?>favicon/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="96x96" href="<?php echo asset_url(); ?>favicon/favicon-96x96.png">
         <link rel="icon" type="image/png" sizes="16x16" href="<?php echo asset_url(); ?>favicon/favicon-16x16.png">
-        <link rel="manifest" href="/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="<?php echo asset_url(); ?>favicon/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
